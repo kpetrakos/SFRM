8ab3e84373ed56900350f8357f550a14351545a3
panagiotislymberis <59731289+panagiotislymberis@users.noreply.github.com>
2020-01-10 13:28:02 +0200

    ghfgh
    ghfgh

--- BalanceContracts.sql
+++ BalanceContracts.sql
@@ -1,7 +1,7 @@
 spool BalanceContracts.log
 set serveroutput on size 100000;
 
-exec SFMR_PACK.Balance_Contracts( &1 );
+exec SFMR_PACK.Balance_Contracts( &1 ); /*  Panos Comment */ 
 
 spool off;
 exit;
